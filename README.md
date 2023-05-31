e4bccee137e6b17f785efa13a3123417836faf5b
Juan Palma <juan.palma@me.com>
2023-05-31 16:35:43 -0600

    entrega 1

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# evan_invitacion
+E-Vangelium - Invitacion Ponencia
+===================
+
+![Ponencia](assets/img/facebook_web_img_article.jpg)
---
+++ assets/js/owner/main.js
@@ -0,0 +1,16 @@
+function iniciar() {
+	
+    
+}
+
+
+
+
+// iniciar la solicitud de los modulos y la ejecucion inicial del sistema.
+//importamos los archivos y librerias necesarios
+requirejs.config({
+	baseUrl: "assets/js/owner",
+	paths: { a: "../animaciones", l: "../librerias", n: "/node_modules"},
+});
+requirejs(["l/modernizr", "precarga", "validaciones", "alertas", "peticiones"], iniciar);
+
